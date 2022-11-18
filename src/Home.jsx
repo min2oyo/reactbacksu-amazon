ff2c2ae3dde368d907c9aa49f14c1d3294d21e54
min2oyo <min2u@outlook.com>
2022-11-18 20:43:33 +0900

    product props 설정

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,9 +10,17 @@ const Home = () => {
         <div className='home_container'>
           <img className='home_image' src='https://images.idgesg.net/images/article/2017/09/firetvad2-100736366-orig.jpg' alt='home_image' />
           <div className='home_row'>
-            <Product />
+            <Product
+              id="2323"
+              title="제품1입니다"
+              price={3000}
+              image="https://en.pimg.jp/024/292/158/1/24292158.jpg"
+              rating={5}
+            />
           </div>
           <div className='home_row'>
+            <Product />
+            <Product />
             <Product />
           </div>
           <div className='home_row'>
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,31 @@
-const Product = () => {
+import './Product.css';
+
+const Product = ({ id, title, image, price, rating }) => {
 
   // 출력
   return (
     <>
       <div className='product'>
-        <h1>취직상품입니다</h1>
+        <div className="product_info">
+          <p>{title}</p>
+          <p className="product_price">
+            <small>가격</small>
+            <strong>{price}</strong>
+            <small>원</small>
+          </p>
+          <div className="product_rating">
+            <p>★</p>
+            <p>★</p>
+            <p>★</p>
+            <p>★</p>
+          </div>
+        </div>
+        <img src={image} alt="" />
+        <button>장바구니에 담기</button>
       </div>
     </>
   );
 
 };
 
-export default Product;
+export default Product;;
