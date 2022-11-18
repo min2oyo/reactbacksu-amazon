a908567c2e089ff22af6782dff48d67e45cb2e43
min2oyo <min2u@outlook.com>
2022-11-18 20:00:24 +0900

    Product.jsx 준비

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import './Home.css';
+import Product from './Product';
 
 const Home = () => {
 
@@ -9,13 +10,13 @@ const Home = () => {
         <div className='home_container'>
           <img className='home_image' src='https://images.idgesg.net/images/article/2017/09/firetvad2-100736366-orig.jpg' alt='home_image' />
           <div className='home_row'>
-            {/* 상품 */}
+            <Product />
           </div>
           <div className='home_row'>
-            {/* 상품 */}
+            <Product />
           </div>
           <div className='home_row'>
-            {/* 상품 */}
+            <Product />
           </div>
         </div>
       </div>
---
+++ src/Product.jsx
@@ -0,0 +1,14 @@
+const Product = () => {
+
+  // 출력
+  return (
+    <>
+      <div className='product'>
+        <h1>취직상품입니다</h1>
+      </div>
+    </>
+  );
+
+};
+
+export default Product;
