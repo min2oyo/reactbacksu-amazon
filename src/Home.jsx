089cd1d6967be61a8b0ddf3363063309f41b1599
min2oyo <min2u@outlook.com>
2022-11-18 19:55:11 +0900

    Home 작업

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,23 @@
+import './Home.css';
+
 const Home = () => {
 
   // 출력
   return (
     <>
       <div className='home'>
-        <h1>본문입니다</h1>
+        <div className='home_container'>
+          <img className='home_image' src='https://images.idgesg.net/images/article/2017/09/firetvad2-100736366-orig.jpg' alt='home_image' />
+          <div className='home_row'>
+            {/* 상품 */}
+          </div>
+          <div className='home_row'>
+            {/* 상품 */}
+          </div>
+          <div className='home_row'>
+            {/* 상품 */}
+          </div>
+        </div>
       </div>
     </>
   );
