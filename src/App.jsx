53cc7ad1291e115ba699129d9449ff5d1df3227a
min2oyo <min2u@outlook.com>
2022-11-18 16:44:33 +0900

    firebase 설정

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  // 출력
+  return (
+    <>
+      <h2>안녕하세요~</h2>
+    </>
+  );
+
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKC9XYFOC8LyVxOjYTmDKoQOTPP2-q3Rw",
+  authDomain: "reactbacksu.firebaseapp.com",
+  projectId: "reactbacksu",
+  storageBucket: "reactbacksu.appspot.com",
+  messagingSenderId: "1078267026166",
+  appId: "1:1078267026166:web:0ae47a6b52b4543ba7aad7",
+  measurementId: "G-5Z5EPGZ384"
+};
